4377a7e6d9842c3c38a041790fdca56160259b09
puptup <putumoney@gmail.com>
2023-02-14 23:27:56 +0300

    feat: add russian lang

--- src/localization/russian/index.ts
+++ src/localization/russian/index.ts
@@ -1,18 +1,246 @@
 export const russianTranslation = {
+  mainLinks: {
+    home: "Домой",
+    solutions: "Решения",
+    aboutUs: "О нас",
+    ourTeam: "Наша команда",
+    services: "Сервисы",
+    blog: "Блог",
+    contacts: "Контакты",
+    faq: "FAQs",
+  },
+  serviceLinks: {
+    pages: "Страницы",
+    elements: "Элементы",
+    pricing: "Цены",
+    siteMap: "Карта сайта",
+  },
+  contacts: {
+    email: "Почта",
+    phone: "Телефон",
+    adress: "Адрес",
+    adressTitle: "1642 Вашингтон-авеню, Джексон, Массачусетс, Миссисипи, 39201",
+  },
+
   header: {
-    menu: {
-      home: "хуй",
-      solutions: "хуй",
-      aboutUs: "хуй",
-      ourTeam: "хуй",
-      services: "хуй",
-      blog: "хуй",
+    solutionsLinks: {
+      dataIntegration: "Интеграция данных",
+      dataVizualization: "Визуализация данных",
+      bigData: "Большие данные",
     },
-    burgerMenu: {
-      home: "хуй",
-      solutions: "хуй",
-      pages: "хуй",
+    watchDemo: "Посмотреть демо",
+  },
+
+  footer: {
+    quickLinks: "Быстрые ссылки",
+    serviceLinks: "Сервисные ссылки",
+    contactInfo: "Контактная информация",
+    privacyPolicy: "Политика конфиденциальности",
+    termsOfUs: "Условия о нас",
+    rightsReserved: "Ensome© 2022 Все права защищены",
+    description:
+      "Открывший счастливой собой чтобы никого то говорил несло, равно картину которое, боль само. Воспользоваться того превратное физическими воспользоваться человек кто картину.",
+  },
+
+  home: {
+    findTruePower: {
+      title: "Find true power in your data with ",
+      ensome: "Ensome",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauda, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.",
+      learnMore: "Learn more",
     },
-    watchDemo: "хуй",
+    analitics: {
+      title: "The newest business analytics platform",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauda, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.",
+      discoverMore: "Discover more",
+    },
+    newSolutions: {
+      title: "Radically new solutions for data",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauda, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.",
+      discoverMore: "Discover more",
+    },
+    ourBlog: {
+      title: "Our blog",
+      learnMore: "Learn more",
+    },
+    testimonials: {
+      title: "Testimonials",
+    },
+  },
+  aboutUs: {
+    pageName: "About us",
+    contactUsSection: {
+      title: "Left questions? Contacts us now for a free consultation and free trial!",
+      description:
+        "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.",
+    },
+    descriptionSection: {
+      whoWeAre: "Who we are",
+      whoWeAreDescription:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Donec tincidunt tempor quam, non mollis quam finibus nec. Quisque finibus consequat felis vel pretium. Aliquam gravida nisi vel convallis ultricies. Integer ante sapien, consequat et dolor vel, cursus lacinia lectus.",
+      ourMission: "Our mission",
+      ourMissionDescription:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Donec tincidunt tempor quam, non mollis quam finibus nec. Quisque finibus consequat felis vel pretium. Aliquam gravida nisi vel convallis ultricies. Integer ante sapien, consequat et dolor vel, cursus lacinia lectus.",
+    },
+    ensomeInNumbers: {
+      title: "Ensome in numbers",
+      projectExecuted: "Project executed",
+      dataAnalytics: "Data analytics",
+      dataManagement: "Data management",
+      satisfiedCustomers: "Satisfied customers",
+    },
+    testimonialsSection: {
+      title: "Testimonials",
+    },
+    mobileHeadSection: {
+      pageName: "About us",
+      title: "We work with 15 years of experience",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+    },
+    ourCustomersSection: {
+      title: "Our customers",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+    },
+  },
+  solutions: {
+    pageName: "Solutions",
+    pageTitle: "Data analitics solutions",
+    pageDescription: "Getting ready for your success, we provide truly outstanding IT solutions.",
+    testimonialsSection: {
+      title: "Testimonials",
+    },
+    chooseUsSection: {
+      title: "Why choose us?",
+      machineLearning: "Machine learning",
+      machineLearningDescription:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+      embedAnalytics: "Embed analytics",
+      embedAnalyticsDescription:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+      accessControl: "Access control",
+      accessControlDescription:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+    },
+  },
+  services: {
+    pageName: "Services",
+    pageTitle: "Data Analytics Services",
+    pageDescription:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+  },
+  servicePost: {
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+    services: {
+      title: "Services",
+    },
+    searchBar: {
+      search: "Search",
+      nofingFound: "Nothing found",
+    },
+    concactInformation: {
+      title: "Contact information",
+      description: "Fill up the form and our Team will get back to you with 25 hours.",
+    },
+  },
+  ourTeam: {
+    pageName: "Our team",
+    title: "Get to know us closer",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+  },
+  member: {
+    memberSection: {
+      name: "Name",
+      position: "Position",
+      decription: "Description",
+      socialNetworks: "Social networks",
+    },
+    contactsSection: {
+      title: "Want {{name}} to share his experience with you?",
+    },
+  },
+  faqs: {
+    pageName: "FAQs",
+    pageTitle: "Frequently asked questions",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+  },
+  contactsPage: {
+    title: "How can we help you?",
+  },
+  blog: {
+    pageName: "Blog",
+    pageTitle: "Blog",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+    moreArticles: "More articles",
+  },
+  blogPost: {
+    searchButton: "Search",
+    relatedPosts: "Related Posts",
+    popularPosts: "Popular Posts",
+    categories: "Categories",
+    views: "Views",
+    tags: "Tags",
+  },
+
+  benefits: {
+    title: "The benefits of Ensome",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+  },
+
+  contactUsForm: {
+    contactUs: "Contact us",
+    placeHolder: {
+      name: "Your name",
+      email: "Your email",
+      theme: "Theme",
+      message: "Your message",
+    },
+    description: {
+      name: "Name",
+      email: "Email",
+      theme: "Theme",
+      message: "Message",
+    },
+    send: "Send",
+    submit: "Submit",
+  },
+
+  needHelpSection: {
+    title: "Do you need help?",
+    description:
+      "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi.",
+    contactUs: "Contact us",
+  },
+
+  pricingSection: {
+    ourPricing: "Our pricing",
+    plan: "Plan",
+    price: "Price",
+  },
+
+  statisticsSection: {
+    title: "We provide services that guarantee your success",
+    projectExecuted: "Project executed",
+    dataManagement: "Data management",
+    satisfiedCustomers: "Satisfied customers",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+  },
+
+  subscribeSection: {
+    title: "Subscribe to our newsletter",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.",
+    placeholder: "Your email",
+    send: "Send",
   },
 };
